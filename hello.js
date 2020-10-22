bc4497721fbf9b60fa8b127ac94f1c80dce54a2e
jtreeves <jackson.t.reeves@gmail.com>
2020-10-22 10:00:36 -0400

    [js] Update to a full 10 tests

--- hello.js
+++ hello.js
@@ -8,3 +8,8 @@ console.log(sayHello('Tom'));
 console.log(sayHello('Mary'));
 console.log(sayHello('Susan'));
 console.log(sayHello('Adam'));
+console.log(sayHello('Veronica'));
+console.log(sayHello('LeBron'));
+console.log(sayHello('Marissa'));
+console.log(sayHello('Norman'));
+console.log(sayHello('Lawrence'));
