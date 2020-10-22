b966b1a58c76557ef080e26e68f7c4a9bd2c9cbb
jtreeves <jackson.t.reeves@gmail.com>
2020-10-22 09:57:23 -0400

    [js] Create 5 tests

---
+++ hello.js
@@ -0,0 +1,10 @@
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('Jackson'));
+console.log(sayHello('Tom'));
+console.log(sayHello('Mary'));
+console.log(sayHello('Susan'));
+console.log(sayHello('Adam'));
