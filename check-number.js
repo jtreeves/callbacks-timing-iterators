611125ecd70507aeb08982630635bb4287ea37f3
jtreeves <jackson.t.reeves@gmail.com>
2020-10-22 10:10:32 -0400

    [js] Add 5 more tests for check number

--- check-number.js
+++ check-number.js
@@ -13,3 +13,8 @@ console.log(checkNumber(-11));
 console.log(checkNumber(0));
 console.log(checkNumber(57));
 console.log(checkNumber(-23));
+console.log(checkNumber(76));
+console.log(checkNumber(0.78));
+console.log(checkNumber(-8.93));
+console.log(checkNumber(0.0000));
+console.log(checkNumber(123.456));
